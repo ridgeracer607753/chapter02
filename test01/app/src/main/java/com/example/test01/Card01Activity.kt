5d89bb5698ec6e72c4225f750fb4fe3b79abc9da
P040631 <>
2025-02-20 09:46:11 +0900

    이미지 설정

---
+++ test01/app/src/main/java/com/example/test01/Card01Activity.kt
@@ -0,0 +1,15 @@
+package com.example.test01
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Card01Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card01)
+
+    }
+}
--- test01/app/src/main/java/com/example/test01/SlashActivity.kt
+++ test01/app/src/main/java/com/example/test01/SlashActivity.kt
@@ -3,25 +3,22 @@ package com.example.test01
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.delay
 
-class MainActivity : AppCompatActivity() {
+@Suppress("DEPRECATION")
+class SlashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        setContentView(R.layout.activity_slash)
 
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } , 3000)
 
-        var image1 = findViewById<ImageView>(R.id.card01)
-        image1.setOnClickListener {
-            println("123213")
-            var intent = Intent(this, Card01Activity::class.java)
-            startActivity(intent)
-        }
     }
 }
